7b6fcda9ac0128d9f92546518b5370fe35353d2d
203421 <78715426+203421@users.noreply.github.com>
2023-03-28 21:22:05 -0600

    a
    a

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,16 @@
 import React  from "react"
 import ReactDOM from 'react-dom/client'
 import App from './containers/App'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './Assets/index.css';
+import App from './Components/App';
+import reportWebVitals from './reportWebVitals';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+  reportWebVitals()
+);
